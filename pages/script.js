54bdab8b48141748e9d42de4bb8531741ab5232d
CaioAlberto97 <110138209+CaioAlberto97@users.noreply.github.com>
2022-09-25 18:57:39 -0300

    Add files via upload

---
+++ pages/script.js
@@ -0,0 +1,17 @@
+const menuButton = document.querySelector('.menu')
+
+menuButton.addEventListener('click', function(){
+    console.log(menuButton.src)
+    menuButton
+    const ul = document.querySelector('ul')
+    ul.classList.toggle('show')
+
+    const div = document.querySelector('div')
+    div.classList.toggle('show-div')
+
+    if (menuButton.src == 'http://127.0.0.1:5500/imgs/menu.png'){
+        menuButton.src = `/imgs/close.png`
+    } else {
+        menuButton.src = "/imgs/menu.png"
+    }
+})
